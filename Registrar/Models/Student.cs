541efdca130f32f58102b7d479793cbb599376e2
chrisdfghjkl <cdepastene@gmail.com>
2022-03-15 19:21:45 -0700

    add property

--- Registrar/Models/Student.cs
+++ Registrar/Models/Student.cs
@@ -11,6 +11,7 @@ namespace Registrar.Models
 
         public int StudentId { get; set; }
         public string Name { get; set; }
+        public DateTime Date { get; set; }
 
         public virtual ICollection<CourseStudent> JoinEntities { get;}
     }
