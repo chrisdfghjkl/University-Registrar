d4750f15a04e5593be12d628c766f1fe5426c4a9
chrisdfghjkl <cdepastene@gmail.com>
2022-03-15 19:27:26 -0700

    Add next migration

---
+++ Registrar/Migrations/20220316022526_DateCourseNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Registrar.Migrations
+{
+    public partial class DateCourseNumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Students",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "CourseNumber",
+                table: "Courses",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "CourseNumber",
+                table: "Courses");
+        }
+    }
+}
