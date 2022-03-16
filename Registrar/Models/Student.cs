a69e1c5c7ba30919ce292f6ce87467d7f4d9e3ac
chrisdfghjkl <cdepastene@gmail.com>
2022-03-15 19:06:36 -0700

    update model naming convention

--- Registrar/Models/Student.cs
+++ Registrar/Models/Student.cs
@@ -10,7 +10,7 @@ namespace Registrar.Models
         }
 
         public int StudentId { get; set; }
-        public string Description { get; set; }
+        public string Name { get; set; }
 
         public virtual ICollection<CourseStudent> JoinEntities { get;}
     }
