8559aa41312c103bdabcafbd88059c16ceae2fc6
chrisdfghjkl <cdepastene@gmail.com>
2022-03-16 20:32:20 -0700

    Add migrations

---
+++ Registrar/Migrations/20220317031108_Department.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Registrar.Migrations
+{
+    public partial class Department : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Date",
+                table: "Students",
+                type: "longtext CHARACTER SET utf8mb4",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.CreateTable(
+                name: "CourseDepartmentStudent",
+                columns: table => new
+                {
+                    CourseDepartmentStudentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    CourseId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseDepartmentStudent", x => x.CourseDepartmentStudentId);
+                    table.ForeignKey(
+                        name: "FK_CourseDepartmentStudent_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "CourseId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseDepartmentStudent_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "StudentId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseDepartmentStudent_CourseId",
+                table: "CourseDepartmentStudent",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseDepartmentStudent_StudentId",
+                table: "CourseDepartmentStudent",
+                column: "StudentId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseDepartmentStudent");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Date",
+                table: "Students",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(string),
+                oldType: "longtext CHARACTER SET utf8mb4",
+                oldNullable: true);
+        }
+    }
+}
---
+++ Registrar/Migrations/20220317032827_DepartmentFixtwo.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Registrar.Migrations
+{
+    public partial class DepartmentFixtwo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DepartmentId",
+                table: "CourseDepartmentStudent",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    DepartmentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext CHARACTER SET utf8mb4", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.DepartmentId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseDepartmentStudent_DepartmentId",
+                table: "CourseDepartmentStudent",
+                column: "DepartmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_CourseDepartmentStudent_Department_DepartmentId",
+                table: "CourseDepartmentStudent",
+                column: "DepartmentId",
+                principalTable: "Department",
+                principalColumn: "DepartmentId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_CourseDepartmentStudent_Department_DepartmentId",
+                table: "CourseDepartmentStudent");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+
+            migrationBuilder.DropIndex(
+                name: "IX_CourseDepartmentStudent_DepartmentId",
+                table: "CourseDepartmentStudent");
+
+            migrationBuilder.DropColumn(
+                name: "DepartmentId",
+                table: "CourseDepartmentStudent");
+        }
+    }
+}
